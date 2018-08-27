4635199c40370148f2be22d62fcbb940655d7f16
Olga Karaseva <olga@touchtribe.nl>
2018-08-27 23:10:56 +0200

    on item click show details

--- src/components/card.js
+++ src/components/card.js
@@ -2,17 +2,46 @@ import React from 'react'
 
 class Card extends React.Component {
 
+  state = {
+    isExpand: false
+  }
+
   render() {
     const { item } = this.props
+    const { isExpand } = this.state
     return (
       <div className="card" onClick={this.onClick}>
         <div>{item.name}</div>
+        {isExpand && this.renderDetails()}
       </div>
     )
   }
 
   onClick = () => {
-    console.log('onclick')
+    this.setState({
+      isExpand: !this.state.isExpand
+    })
+  }
+
+  renderDetails = () => {
+    const { item } = this.props
+
+    return (
+      <ul className="card__list">
+        <li className="card__item">
+          <span>Gender</span>
+          <span>{item.gender}</span>
+        </li>
+        <li className="card__item">
+          <span>Height</span>
+          <span>{item.height}</span>
+        </li>
+        <li className="card__item">
+          <span>Weight</span>
+          <span>{item.mass}</span>
+        </li>
+      </ul>
+    )
   }
 
 }
